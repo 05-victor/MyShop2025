9b0c2bae3ae55df95e06d8461ef3732ad35d3e3c
Le Tan Hiep <tanhiep24135@gmail.com>
2025-10-22 23:53:00 +0700

    Fix when login with < 6 chars

--- src/MyShop.Client/ViewModels/LoginViewModel.cs
+++ src/MyShop.Client/ViewModels/LoginViewModel.cs
@@ -34,6 +34,7 @@ namespace MyShop.Client.ViewModels {
 
         [ObservableProperty]
         [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters")]
         private string _password = string.Empty;
 
         [ObservableProperty]
