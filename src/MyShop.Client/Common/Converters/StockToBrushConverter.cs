a77db01270e71b960d18c89d2a98f110439e7e5a
TonKnight <tanhiep24135@gmail.com>
2025-12-14 21:32:12 +0700

    chore: Integrate StockToBrushConverter in AdminReportsPage

--- src/MyShop.Client/Common/Converters/StockToBrushConverter.cs
+++ src/MyShop.Client/Common/Converters/StockToBrushConverter.cs
@@ -18,17 +18,17 @@ public class StockToBrushConverter : IValueConverter
         }
 
         var colorType = parameter as string ?? "Background";
-        
+
         return (stock, colorType) switch
         {
             // Out of Stock (0)
             (0, "Background") => new SolidColorBrush(Color.FromArgb(255, 254, 226, 226)), // #FEE2E2
             (0, "Foreground") => new SolidColorBrush(Color.FromArgb(255, 185, 28, 28)),   // #B91C1C
-            
+
             // Low Stock (< 10)
-            (< 10, "Background") => new SolidColorBrush(Color.FromArgb(255, 254, 243, 199)), // #FEF3C7
-            (< 10, "Foreground") => new SolidColorBrush(Color.FromArgb(255, 180, 83, 9)),    // #B45309
-            
+            ( < 10, "Background") => new SolidColorBrush(Color.FromArgb(255, 254, 243, 199)), // #FEF3C7
+            ( < 10, "Foreground") => new SolidColorBrush(Color.FromArgb(255, 180, 83, 9)),    // #B45309
+
             // In Stock (>= 10)
             (_, "Background") => new SolidColorBrush(Color.FromArgb(255, 209, 250, 229)), // #D1FAE5
             (_, "Foreground") => new SolidColorBrush(Color.FromArgb(255, 5, 150, 105)),   // #059669
